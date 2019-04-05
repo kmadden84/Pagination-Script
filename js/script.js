f92de72239e6f6d67d44fece3da41256db6a368e
kmadden84 <49003301+kmadden84@users.noreply.github.com>
2019-04-05 17:09:01 -0400

    Update script.js
    updating script to use range of values calculated by page number, instead of pushing to array and calling by array number

--- js/script.js
+++ js/script.js
@@ -2,84 +2,104 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
+
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
 // Global Variables
 
 const searchInput = document.getElementById('searchInput');
-const ul = document.querySelector('.student-list');
-let studentItems = ul.querySelectorAll('.student-item');           
+let allStudentItems = document.querySelectorAll('.student-item');
+let ul = document.querySelector('.student-list');
 const toggleContainer = document.getElementById('cycle');
-const totalSize = studentItems.length;
-let groupedArray = [];
 const count = 10;
-
+const studentClass = 'student-item';
+const hidden = 'js-hidden';
+let noItems;
 
 // Looping through items, pushing to array, appending buttons
 
+console.log(ul);
 
-function loopItems() {
-    let studentItems = ul.querySelectorAll('.student-item');    // Recheck list to get all list items with student-item calss
-    const studentItemsArray = Array.from(studentItems);         // Convert list of student items to array (to use slice method)
-    groupedArray = [];                                          // Empty the array of student items every time loopItems is called (to reset after search)
-    for (i = 0; i < studentItemsArray.length; i += count) {     // loop through all student items
-        let groupSize = studentItemsArray.slice(i, i + count);  // slice into groups of 10
-        groupedArray.push(groupSize);                           // push these groups of 10 into the groupedArray (2D array)
+function displayItems(page) {
+    const pageIndex = typeof(page) === 'number' ? page : 0; // pageIndex starts at 0 if not specified, otherwise pull from text value of button
+    let studentItems = document.querySelectorAll('.student-item'); // Recheck list to get all list items with student-item calss
+    const start = pageIndex * count; // start of item range
+    const end = (pageIndex + 1) * count; //end of item range
+
+    //iF there are no items, show no items found
+
+    if (studentItems.length === 0) {
+        const noItemsEl = document.getElementById('noitems');
+        if (!noItemsEl) {
+        let noItems = document.createElement('span');
+        noItems.id = 'noitems';
+        noItems.textContent = 'no items found';
+        ul.append(noItems);
     }
-    $(studentItems).hide();                                     //hide all items
-    $(groupedArray[0]).show();                                  //show first array in 2D array
-    createButtons()                                             //call function to create/recreate buttons whenever items are looped
+    } else {
+
+        for (i = 0; i < studentItems.length; i++) { // loop through all student items
+            //debugger;
+            if (i >= start && i < end) { // if within range of items we want to show, show them
+                studentItems[i].classList.remove(hidden);
+            } else {
+                studentItems[i].classList.add(hidden);  // if outside of range, hide them
+            }
+            const noItems = document.getElementById('noitems');
+            console.log(noItems);
+            if (noItems) {              
+                ul.removeChild(noItems); // if "no items found" warning exists, hide it
+            }
+        }
+    }
+    createButtons(); //create page number bbuttons
 }
-loopItems();                                                    //call looped items function on first page load
+displayItems(); //call looped items function on first page load
+
 
 //Search functionality
 
 $('input[type="text"]').on('keyup', function() {
     // Declare variables;
-    filter = searchInput.value.toUpperCase();                      // define search content as the value of the input
-    for (i = 0; i < studentItems.length; i++) {                    // Loop through student-items               
-        let name = studentItems[i].getElementsByTagName("h3")[0];  // define container containing what we're looking for
-        txtValue = name.textContent;                               // define what we're looking for as the text content
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {         // if what we're looking for is an index of the input value 
-            studentItems[i].style.display = "";                    // display default
-            $(studentItems[i]).addClass('student-item');           // add student-item class, for inclusion in array
-            //console.log(studentItemsArray[i].style.display = "");
+    filter = searchInput.value.toUpperCase(); // define search content as the value of the input
+    for (i = 0; i < allStudentItems.length; i++) { // Loop through student-items               
+        let name = allStudentItems[i].getElementsByTagName("h3")[0]; // define container containing what we're looking for
+        txtValue = name.textContent; // define what we're looking for as the text content
+        if (txtValue.toUpperCase().indexOf(filter) > -1) { // if what we're looking for is an index of the input value 
+            allStudentItems[i].classList.add(studentClass); // add student-item class, for inclusion in range
+            allStudentItems[i].classList.remove(hidden);
         } else {
-            studentItems[i].style.display = "none";                // otherwise display none
-            $(studentItems[i]).removeClass('student-item');       // and remove class student-items, for removal from array
+            allStudentItems[i].classList.remove(studentClass); // otherwise, remove student-item class and hide items
+            allStudentItems[i].classList.add(hidden); // and add class hidden to non-matching items
         }
     }
-    loopItems();                                                   /* call the loop items function on every search, so we can re-check 
-                                                                      how many student-items there are, and redefine what goes in the array */
+    displayItems();
 })
 
 
 // Creating and Appending page links
 
 function createButtons() {
-    $('#cycle span').remove();                                  //remove all page buttons (to reset after search)
-    groupedArray.forEach(function(value, i) {                   //cicle through every array of 10 student-items
-        let pageNum = i;                                        //define page Number
-        pageNum = parseInt(pageNum);                            //convert page Number to integer
-        pageNum = pageNum + 1;                                  //add 1 to page number
-        let span = document.createElement('span');              //create span to house button with page number
-        span.textContent = pageNum;                             //define text as housing the page number
-        toggleContainer.appendChild(span);                      //append a page button for every grouping of 10
-    })
+    let studentItems = document.querySelectorAll('.student-item'); //find all items with class "student-item"
+    toggleContainer.innerHTML = ''; //remove all page buttons (to reset after search)
+    for (i = 1; i <= Math.ceil(studentItems.length / count); i++) { // loop through all studen items
+        let pageNum = i; //define page Number
+        let span = document.createElement('span'); //create span to house button with page number
+        span.textContent = pageNum; //define text as housing the page number
+        toggleContainer.appendChild(span); //append a page button for every grouping of 10
+    }
 }
 
 
 // Define what items to show when page numbers are clicked
 
 toggleContainer.addEventListener('click', (e) => {
-    let button = event.target;                                  // target what is clicked
-    if (button.tagName === 'SPAN') {                            // target specifically the spans holding the page numbers
-        let index = button.textContent;                         // define the array index as the text in the button
-        index = parseInt(index);                                // parse text as integer
-        index = index - 1;                                      // subtract 1 from interger
-        $(studentItems).hide();                                 // hide entire array
-        $(groupedArray[index]).show();                          // show only student-items group with specififed index
+    let button = event.target; // target what is clicked
+    if (button.tagName === 'SPAN') {
+        let index = button.textContent; // define the array index as the text in the button
+        index = parseInt(index); // parse text as integer
+        index = index - 1; // subtract 1 from the value of the text button, so we start at 0
+        displayItems(index); //call displayItems function with the value of the button
     }
 });
